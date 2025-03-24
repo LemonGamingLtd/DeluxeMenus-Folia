0d175c6fe4f99cdfe27041cea0ed1d7220e406d8
Nahuel Dolores <contact@nahu.me>
2025-03-24 14:57:39 +0100

    Add close inventory handler to DupeFixer Co-authored-by: R00tB33rMan

--- src/main/java/com/extendedclip/deluxemenus/dupe/DupeFixer.java
+++ src/main/java/com/extendedclip/deluxemenus/dupe/DupeFixer.java
@@ -3,9 +3,12 @@ package com.extendedclip.deluxemenus.dupe;
 import com.extendedclip.deluxemenus.DeluxeMenus;
 import com.extendedclip.deluxemenus.listener.Listener;
 import com.extendedclip.deluxemenus.utils.DebugLevel;
+import io.github.projectunified.minelib.scheduler.entity.EntityScheduler;
 import io.github.projectunified.minelib.scheduler.global.GlobalScheduler;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
@@ -24,6 +27,49 @@ public class DupeFixer extends Listener {
     public DupeFixer(@NotNull final DeluxeMenus plugin, @NotNull final MenuItemMarker marker) {
         super(plugin);
         this.marker = marker;
+    }
+
+    @EventHandler
+    private void onInventoryClose(@NotNull final InventoryCloseEvent event) {
+        if (!(event.getPlayer() instanceof Player)) {
+            return;
+        }
+
+        final Player player = (Player) event.getPlayer();
+        EntityScheduler.get(DeluxeMenus.getInstance(), player).runLater(() -> {
+            boolean removed = false;
+
+            for (ItemStack itemStack : player.getInventory().getContents()) {
+                if (itemStack == null || !marker.isMarked(itemStack)) {
+                    continue;
+                }
+
+                player.getInventory().remove(itemStack);
+                removed = true;
+
+                plugin.debug(
+                        DebugLevel.LOWEST,
+                        Level.INFO,
+                        "DeluxeMenus item found in main inventory on close. Removing it."
+                );
+            }
+
+            ItemStack offhand = player.getInventory().getItemInOffHand();
+            if (marker.isMarked(offhand)) {
+                player.getInventory().setItemInOffHand(null);
+                removed = true;
+
+                plugin.debug(
+                        DebugLevel.LOWEST,
+                        Level.INFO,
+                        "DeluxeMenus item found in offhand on close. Removing it."
+                );
+            }
+
+            if (removed) {
+                player.updateInventory();
+            }
+        }, 1L);
     }
 
     @EventHandler
